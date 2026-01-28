454d61ead297ea3960a045ca3ffb5b9baf33bf99
KiloTheNeko <slimejackpl@gmail.com>
2026-01-29 00:57:37 +0100

    Created project files and started creating project

---
+++ Zad_Zaklad_V1/WorkerClass.cs
@@ -0,0 +1,19 @@
+﻿namespace Zad_Zaklad_V1;
+
+public enum Position
+{
+    Director,
+    Manager,
+    Security,
+    Janitor
+}
+
+public sealed class Worker(int id, string firstName, string lastName, int salary, Position position)
+{
+    public int Id { get; } = id;
+    public string FirstName { get; } = firstName;
+    public string LastName { get; } = lastName;
+    public int Salary { get; } = salary;
+    public Position Position { get; } = position;
+}
+
---
+++ Zad_Zaklad_V1/WorkerMethods.cs
@@ -0,0 +1,14 @@
+﻿namespace Zad_Zaklad_V1;
+
+public static class WorkerMethods
+{
+    public static void Display(this IEnumerable<Worker> workers)
+    {
+        
+    }
+    
+    public static int SalaryByPosition(this IEnumerable<Worker> workers, Position position)
+    {
+        return 0;
+    }
+}
