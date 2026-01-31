69d15013cfa3971b0c81213dac5e7cba481495e0
Hubert <126041@student.san.edu.pl>
2026-01-31 20:04:02 +0100

    dodana metoda

--- Zad_Zaklad_V1/WorkerMethods.cs
+++ Zad_Zaklad_V1/WorkerMethods.cs
@@ -4,7 +4,16 @@ public static class WorkerMethods
 {
     public static void Display(this IEnumerable<Worker> workers)
     {
-        
+        foreach (var worker in workers)
+    {
+        Console.WriteLine(
+            $"ID: {worker.Id}, " +
+            $"Name: {worker.FirstName}, " +
+            $"LastName: {worker.LastName}, " +
+            $"Position: {worker.Position}, " +
+            $"Salary: {worker.Salary:C}"
+        );
+    }
     }
     
     public static decimal SalaryByPosition(this IEnumerable<Worker> workers, Position position)
